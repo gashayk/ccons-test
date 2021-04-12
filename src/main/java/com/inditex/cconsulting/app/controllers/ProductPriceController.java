722c82c545d9eca42e9670adb5cc7609205d7f49
Hayk <gashayk@yahoo.com>
2021-04-12 15:54:00 +0200

    Changed request param in ProductPriceController

--- src/main/java/com/inditex/cconsulting/app/controllers/ProductPriceController.java
+++ src/main/java/com/inditex/cconsulting/app/controllers/ProductPriceController.java
@@ -1,5 +1,7 @@
 package com.inditex.cconsulting.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inditex.cconsulting.app.AppUtils;
 import com.inditex.cconsulting.app.Exceptions.CustomException;
 import com.inditex.cconsulting.app.model.ProductPriceResponse;
@@ -12,9 +14,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,20 +30,28 @@ public class ProductPriceController {
     private final Logger logger = LoggerFactory.getLogger(ProductPriceController.class);
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private PricingService pricingService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Find product price")
-    public ResponseEntity<?> findProductPrice(ProductPriceRequest request) throws CustomException {
+    public ResponseEntity<?> findProductPrice(@RequestParam String request) throws CustomException {
         logger.info("findProductPrice() -> Fetching product");
-
+        ProductPriceRequest requestObject = null;
         if (request == null) {
             logger.error("findProductPrice() -> Request param is null");
             throw new CustomException("Request parameter can't be null", HttpStatus.BAD_REQUEST);
         }
+        try {
+            requestObject = objectMapper.readValue(request, ProductPriceRequest.class);
+        } catch (JsonProcessingException e) {
+            logger.error("findProductPrice() -> JSON conversion error.");
+            throw new CustomException("Unable to convert from json to object", HttpStatus.BAD_REQUEST);
+        }
+        ProductPriceResponse response = pricingService.findProductPrice(AppUtils.convertDTOToEntity(requestObject));
 
-        ProductPriceResponse response = pricingService.findProductPrice(AppUtils.convertDTOToEntity(request));
-
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
